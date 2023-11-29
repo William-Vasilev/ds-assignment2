6c8b243f7fb8610d0e31d1418988a2a5d8b0d1bb
williamvasilev <74902089+William-Vasilev@users.noreply.github.com>
2023-11-29 10:14:08 +0000

    Completed Fan out implementation - added emailing logic!

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -27,6 +27,10 @@ export class EDAAppStack extends cdk.Stack {
       receiveMessageWaitTime: cdk.Duration.seconds(10),
     });
 
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
 
     const newImageTopic = new sns.Topic(this, "NewImageTopic", {
       displayName: "New Image topic",
@@ -35,6 +39,10 @@ export class EDAAppStack extends cdk.Stack {
     newImageTopic.addSubscription(
       new subs.SqsSubscription(imageProcessQueue)
     );
+
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+
     // Lambda functions
 
     const processImageFn = new lambdanode.NodejsFunction(
@@ -49,6 +57,13 @@ export class EDAAppStack extends cdk.Stack {
       }
     );
 
+    const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
+
     // Event triggers
 
     imagesBucket.addEventNotification(
@@ -61,12 +76,31 @@ export class EDAAppStack extends cdk.Stack {
       maxBatchingWindow: cdk.Duration.seconds(10),
     });
 
+    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(10),
+    }); 
+
+    mailerFn.addEventSource(newImageMailEventSource);
+
     processImageFn.addEventSource(newImageEventSource);
 
     // Permissions
 
     imagesBucket.grantRead(processImageFn);
 
+    mailerFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ses:SendEmail",
+          "ses:SendRawEmail",
+          "ses:SendTemplatedEmail",
+        ],
+        resources: ["*"],
+      })
+    );
+    
     // Output
     
     new cdk.CfnOutput(this, "bucketName", {
